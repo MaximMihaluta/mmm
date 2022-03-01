be5a66175874defdbbe73e6fd19cf8aec4e7b7bc
maximmihaluta <maxaimka8383@gmail.com>
2022-03-01 16:41:28 -0600

    latest change

--- Trasformers/src/B.java
+++ Trasformers/src/B.java
@@ -1,10 +1,17 @@
 public class B {
     public static void main(String[] args) {
+/*
 
-     /*  *//* a obj = new a();
+        */
+/*  *//*
+*/
+/* a obj = new a();
         obj.adTwoNu(20,30);*//*
+*/
+/*
         a.adTwoNu(100, 20);
-        a.substrTwoNu(34, 55);*/
+        a.substrTwoNu(34, 55);*//*
+
 
         a.printUntil(10);
         a.printUntil(3);
@@ -13,9 +20,34 @@ public class B {
 
 
         MethodsBasicPartTwoo ggg = new MethodsBasicPartTwoo();
-       int www  =  ggg.calculatett(1998);
+        int www = ggg.calculatett(1998);
         System.out.println(www);
+*/
 
+
+        int[] num = {22, 33, 44, 55, 66};
+      int result = findIdex(num,449);
+        System.out.println(result);
+
+
+    }
+
+    static int findIdex(int[] numArr, int value) {
+        int index = 0;
+        if (numArr.length == 0) {
+            return -1;
+        }
+
+
+        for (int i = 0; i < numArr.length; i++) {
+
+            if (numArr[i] == value) {
+                index = i;
+                return  index;
+            }
+
+        }
+        return index;
 
     }
 }
